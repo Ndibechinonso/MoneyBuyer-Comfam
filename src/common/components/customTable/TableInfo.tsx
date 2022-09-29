5e41b995f5f751508ef2d1c3c2cfc199a8844cee
jidoggs <jidoggs@gmail.com>
2022-09-29 13:13:51 +0100

    transaction paginated

--- src/common/components/customTable/TableInfo.tsx
+++ src/common/components/customTable/TableInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import ThreeDotIcon from "../CustomIcons/ThreeDot";
 import Tag from "../CustomTags";
 import DropDown from "../DropDowns/primitive";
@@ -9,8 +9,10 @@ import clientImg from "../../../static/images/client_img.svg";
 import { toNaira } from "../../utils/helpers";
 import PaginationComponent from "../PaginationComponent";
 import { changePageNumber } from "../redux/disputes/disputesSlice";
+import { changePageNumber as trans_changePageNumber } from "../redux/transaction/transactionSlice";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import CustomLoader from "../CustomLoader";
+import { useLocation } from "react-router-dom";
 
 function TableInfo({
   headers,
@@ -18,19 +20,38 @@ function TableInfo({
   activeOrder,
   recentTransacionHistory,
 }: TObj) {
+  const dispatch = useAppDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [neglectlimit, setLimit] = useState(5);
+  const { pathname } = useLocation();
 
-  const dispatch = useAppDispatch()
-  const [currentPage, setCurrentPage] = useState(1)
-  const [neglectlimit, setLimit] = useState(5)
-  
-  const {loading, count} = useAppSelector((state) => state.disputes)
+  const { loading: dispute_loading, count: dispute_count } = useAppSelector(
+    (state) => state.disputes
+  );
+  const { loading: transaction_loading, count: transaction_count } =
+    useAppSelector((state) => state.transactions);
 
   // const offset = (page - 1) * limit;
   // const endposition = page * limit;
 
-  useEffect(() =>{
-    dispatch(changePageNumber(currentPage-1))
-  }, [currentPage])
+  const loading = dispute_loading || transaction_loading;
+
+  const totalPages = useMemo(
+    () =>
+      pathname.includes("dispute")
+        ? Math.ceil((dispute_count || 0) / 10)
+        : Math.ceil((transaction_count || 0) / 10),
+    [dispute_count, transaction_count, pathname]
+  );
+
+  useEffect(() => {
+    if (pathname.includes("dispute")) {
+      dispatch(changePageNumber(currentPage - 1));
+    }
+    if (pathname.includes("transaction")) {
+      dispatch(trans_changePageNumber(currentPage - 1));
+    }
+  }, [currentPage]); // eslint-disable-line
 
   const tableContentHandler = (row: any, col: any) => {
     let template: any;
@@ -60,11 +81,11 @@ function TableInfo({
         ]?.first_name.substring(0, 1)}.`;
         break;
       case "_id":
-        template = `${row[col.key]}`
+        template = `${row[col.key]}`;
         break;
-        case "dispute_reason":
-          template = `${row[col.key]}`
-          break;     
+      case "dispute_reason":
+        template = `${row[col.key]}`;
+        break;
       default:
         template = row[col.key];
         break;
@@ -74,57 +95,70 @@ function TableInfo({
 
   return (
     <div>
-    <table className="table__display">
-      <thead>
-        <tr>
-          {headers.map((itm) => {
-            return (
-              <th
-                key={itm.title}
-                className={`${itm.title === "Clients" ? "text-left" : ""}`}
-              >
-                {itm.title}
-              </th>
-            );
-          })}
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-       {loading ? <tr className="m-auto"><td colSpan={6}><CustomLoader size={10} /></td></tr> :  data.length > 1 ? data.map((row, idx) => {
-          return (
-            <tr key={idx}>
-              {headers.map((col, id) => (
-                <td key={id} className="">{tableContentHandler(row, col)} </td>
-              ))}
-              <td className="">
-                {!activeOrder || recentTransacionHistory ? (
-                  <DropDown content={<TableItem data={row} />}>
-                    <button>
-                      <ThreeDotIcon />
-                    </button>
-                  </DropDown>
-                ) : null}
-                {activeOrder && !recentTransacionHistory ? (
-                  <ArrowRight />
-                ) : null}
+      <table className="table__display">
+        <thead>
+          <tr>
+            {headers.map((itm) => {
+              return (
+                <th
+                  key={itm.title}
+                  className={`${itm.title === "Clients" ? "text-left" : ""}`}
+                >
+                  {itm.title}
+                </th>
+              );
+            })}
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr className="m-auto">
+              <td colSpan={6}>
+                <CustomLoader size={10} />
               </td>
             </tr>
-          );
-        }) : <tr><td colSpan={6}>No Data</td></tr>}
-      </tbody>
-      
-    </table>
-    <PaginationComponent
-                        loading={false}
-                        currentPage={currentPage}
-                        // totalPages={Math.ceil((count || 0) / limit)}
-                        totalPages={Math.ceil((count || 0) / 10)}
-                        // totalPages={12}
-                        setLimit={setLimit}
-                        setPage={setCurrentPage}
-                        limit={neglectlimit}
-                    />
+          ) : data.length > 1 ? (
+            data.map((row, idx) => {
+              return (
+                <tr key={idx}>
+                  {headers.map((col, id) => (
+                    <td key={id} className="">
+                      {tableContentHandler(row, col)}{" "}
+                    </td>
+                  ))}
+                  <td className="">
+                    {!activeOrder || recentTransacionHistory ? (
+                      <DropDown content={<TableItem data={row} />}>
+                        <button>
+                          <ThreeDotIcon />
+                        </button>
+                      </DropDown>
+                    ) : null}
+                    {activeOrder && !recentTransacionHistory ? (
+                      <ArrowRight />
+                    ) : null}
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={6}>No Data</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <PaginationComponent
+        loading={false}
+        currentPage={currentPage}
+        // totalPages={Math.ceil((count || 0) / limit)}
+        totalPages={totalPages}
+        // totalPages={12}
+        setLimit={setLimit}
+        setPage={setCurrentPage}
+        limit={neglectlimit}
+      />
     </div>
   );
 }
